ea1a698494bb1d2ff8044fb230fdf7c2d0b8dfb7
Clemens Ertle <clemensertle@gmail.com>
2019-05-15 22:04:44 +0200

    posting manually with a fetch request

--- public/javascripts/login.js
+++ public/javascripts/login.js
@@ -4,3 +4,32 @@ $('.message a').click(function () {
         opacity: "toggle"
     }, "slow");
 });
+
+document.querySelector('.login-form').addEventListener('submit', e => {
+    e.preventDefault();
+    submitLogin(e);
+});
+
+
+function submitLogin(e) {
+    fetch('/users/login', {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json',
+        },
+        body: JSON.stringify({
+            email: e.srcElement[0].value,
+            password: e.srcElement[1].value,
+        }),
+    }).then(async (r) => {
+        Swal.fire({
+            position: 'top-end',
+            type: ((r.ok) ? 'success' : 'error'),
+            text: await r.text(),
+            showConfirmButton: false,
+            timer: 3000,
+            background: '#2a2c2e',
+        });
+        if (r.ok) setTimeout(() => document.location = '/users', 1000);
+    });
+}
