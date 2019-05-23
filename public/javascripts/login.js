79b09425eaabca2c58ab52125c5730e7560a9727
Clemens Ertle <clemensertle@gmail.com>
2019-05-23 15:24:38 +0200

    Register request with manual request

--- public/javascripts/login.js
+++ public/javascripts/login.js
@@ -12,6 +12,16 @@ document.querySelector('.login-form').addEventListener('submit', e => {
 });
 
 
+document.querySelector('.login-form').addEventListener('submit', e => {
+    e.preventDefault();
+    submitLogin(e);
+});
+
+document.querySelector('.register-form').addEventListener('submit', e => {
+    e.preventDefault();
+    submitCreate(e);
+});
+
 function submitLogin(e) {
     fetch('/users/login', {
         method: 'POST',
@@ -27,11 +37,36 @@ function submitLogin(e) {
             toast: true,
             position: 'top-end',
             type: ((r.ok) ? 'success' : 'error'),
-            text: await r.text(),
+            text: (r.status !== 503) ? await r.text() : 'Server overloaded, please try again later',
             showConfirmButton: false,
             timer: 3000,
             background: '#2a2c2e',
         });
         if (r.ok) setTimeout(() => document.location = '/users', 1000);
+    });
+}
+
+function submitCreate(e) {
+    fetch('/users/register', {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json',
+        },
+        body: JSON.stringify({
+            username: e.srcElement[0].value,
+            password: e.srcElement[1].value,
+            email: e.srcElement[2].value,
+        }),
+    }).then(async (r) => {
+        Swal.fire({
+            toast: true,
+            position: 'top-end',
+            type: ((r.ok) ? 'success' : 'error'),
+            text: (r.status !== 503) ? await r.text() : 'Server overloaded, please try again later',
+            showConfirmButton: false,
+            timer: 3000,
+            background: '#2a2c2e',
+        });
+        if (r.ok) setTimeout(() => document.location = '/login', 1000);
     });
 }
