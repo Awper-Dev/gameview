975b6fa57ab4ce32c37d94c7fb7f85617ebfa1ac
Clemens Ertle <clemensertle@gmail.com>
2019-05-23 15:25:23 +0200

    Endpoint to Update Code
    Just pulls from the repo

--- routes/index.js
+++ routes/index.js
@@ -1,12 +1,26 @@
 const express = require('express');
 const router = express.Router();
-
+const {
+    exec,
+} = require('child_process');
 // * Main Route, this is the same as /users xD
 router.get('/', function (req, res) {
     res.render('index', {
         title: 'Gameview',
         auth: req.session.auth || false,
         username: req.session.username,
+    });
+});
+
+// ! Updates the whole code if possible, can only be run by owner
+router.get('/update', function (req, res) {
+    if (req.session.email !== process.env.OWNER) return res.status(401).send('endpoint may only be used by owner');
+    exec('git pull origin master', async (err, stdout) => {
+        if (err) throw err;
+        if (stdout === 'Already up-to-date.\n') return res.status(200).send(stdout);
+        console.log('Updated code. restarting now');
+        res.status(200).send('Restarting.');
+        setTimeout(() => process.exit(0), 3000);
     });
 });
 
