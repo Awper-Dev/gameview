3477dcb1291d81bce5cafc3e58967ee70003d8e7
Clemens Ertle <clemensertle@gmail.com>
2019-05-24 16:13:34 +0200

    Rewriting Rendering
    Updating more a small overview

--- public/javascripts/index.js
+++ public/javascripts/index.js
@@ -1,4 +1,27 @@
-/* global Swal */
+/* global $ Swal*/
+const menuIconEl = $('.menu-icon');
+const sidenavEl = $('.sidenav');
+const sidenavCloseEl = $('.sidenav__close-icon');
+
+// Add and remove provided class names
+function toggleClassName(el, className) {
+    if (el.hasClass(className)) {
+        el.removeClass(className);
+    } else {
+        el.addClass(className);
+    }
+}
+
+// Open the side nav on click
+menuIconEl.on('click', function () {
+    toggleClassName(sidenavEl, 'active');
+});
+
+// Close the side nav on click
+sidenavCloseEl.on('click', function () {
+    toggleClassName(sidenavEl, 'active');
+});
+
 // eslint-disable-next-line no-unused-vars
 function submitAddress() {
     Swal.fire({
@@ -39,10 +62,50 @@ function submitAddress() {
     });
 }
 
+// eslint-disable-next-line no-unused-vars
+function removeAddress() {
+    Swal.fire({
+        title: 'Connection you want to remove:',
+        input: 'text',
+        inputAttributes: {
+            autocapitalize: 'off',
+        },
+        showCancelButton: true,
+        confirmButtonText: 'Remove Server',
+        showLoaderOnConfirm: true,
+        preConfirm: (server) => {
+            return fetch('/actions/removeserver', {
+                    method: 'POST',
+                    headers: {
+                        'Content-Type': 'application/json',
+                    },
+                    body: JSON.stringify({
+                        ip: server,
+                    }),
+                })
+                .then(async response => {
+                    if (!response.ok) throw new Error(await response.text());
+                    return response.text();
+                })
+                .catch(error => Swal.showValidationMessage(error));
+        },
+        allowOutsideClick: () => !Swal.isLoading(),
+    }).then((result) => {
+        if (result.value) {
+            Swal.fire({
+                title: 'Removed Server',
+                text: result.value,
+                type: 'info',
+            });
+            setTimeout(() => updateServers(false), 2000);
+        }
+    });
+}
+
 function updateServers(setTimer = true) {
     const table = document.querySelector('#server-table');
-    const div = document.querySelector('#server-container');
-    if (!div) return;
+    const div = document.querySelector('#server-card');
+    const overview = document.querySelector('.main-overview');
     Swal.fire({
         toast: true,
         title: 'Pulling Data',
@@ -57,7 +120,12 @@ function updateServers(setTimer = true) {
     }).then(async (r) => {
         clearTable(table);
         const res = (await r.json());
+        const online = res.filter(x => x.online);
         res.sort((a, b) => a.online - b.online);
+        overview.firstElementChild.classList.remove('offline-card');
+        overview.firstElementChild.classList.remove('online-card');
+        overview.firstElementChild.lastElementChild.innerHTML = `${online.length} / ${res.length} Server Online`;
+        overview.firstElementChild.classList.add((online.length === res.length) ? 'online-card' : 'offline-card');
         res.forEach(element => {
             table.appendChild(renderRow(element));
             div.appendChild(table);
